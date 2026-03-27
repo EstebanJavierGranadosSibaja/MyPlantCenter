96dba9cce6be896eea6b159d29e53c34760e9895
Esteban Javier Granados Sibaja <154352986+EstebanJavierGranadosSibaja@users.noreply.github.com>
2026-03-26 19:28:29 -0600

    fix: reorder import statements for consistency in useAuth and auth.service

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -1,6 +1,6 @@
 import * as Google from 'expo-auth-session/providers/google';
-import * as WebBrowser from 'expo-web-browser';
 import Constants from 'expo-constants';
+import * as WebBrowser from 'expo-web-browser';
 import { useEffect, useState } from 'react';
 
 WebBrowser.maybeCompleteAuthSession();
