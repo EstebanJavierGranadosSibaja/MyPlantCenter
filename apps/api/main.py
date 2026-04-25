9864a19759a15890afa88133b7dcc667fa722390
Esteban Javier Granados Sibaja <154352986+EstebanJavierGranadosSibaja@users.noreply.github.com>
2026-04-24 20:12:51 -0600

    fix: update import path for register_routers to reflect new module structure

--- apps/api/main.py
+++ apps/api/main.py
@@ -6,7 +6,7 @@ from uuid import uuid4
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 
-from apps.api.routers import register_routers
+from routers import register_routers
 
 
 logger = logging.getLogger("myplantcenter.api")
